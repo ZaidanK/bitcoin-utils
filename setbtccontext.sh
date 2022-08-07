86b431c4210d984c0f4f28e9dd87e762deb41f22
amovfx <45839100+amovfx@users.noreply.github.com>
2022-08-07 12:24:47 -0600

    added selction for branch

--- setbtccontext.sh
+++ setbtccontext.sh
@@ -12,17 +12,17 @@ export BRANCH=${a[0]}
 export NETWORK=${a[1]}
 
 cd $GITDIR
-#select what branch
-# branches=()
-# eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
-# select opt in "${branches[@]}"
-# do
-#     echo "You picked $opt ($REPLY)"
-#     SELECTED=$(echo "$opt" | sed 's#refs/heads/##g')
-#     echo $SELECTED
-#     git checkout $SELECTED
-#     break;
-# done
+select what branch
+branches=()
+eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
+select opt in "${branches[@]}"
+do
+    echo "You picked $opt ($REPLY)"
+    SELECTED=$(echo "$opt" | sed 's#refs/heads/##g')
+    echo $SELECTED
+    git checkout $SELECTED
+    break;
+done
 
 # OLD_PROMPT=$PROMPT
 # PROMPT=" (${NETWORK}) ${SELECTED} %1~ %# "
