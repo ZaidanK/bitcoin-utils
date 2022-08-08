284957f836990c3b0e7dbb1825b0e38e07dadffd
amovfx <45839100+amovfx@users.noreply.github.com>
2022-08-08 16:52:26 -0600

    configure, build and set_alias

--- launch.sh
+++ launch.sh
@@ -1,14 +1,67 @@
 #!/bin/zsh
+export GITDIR=/Users/${USER}/git/bitcoin
+export BITCOIN_BUILDDIR=/Users/${USER}/bitcoin/builds/
+export BITCOIN_CHAIN=mainnet
+export BITCOIN_GIT_BRANCH=master
+
 
 setbtccontext() {
-    source /Users/${USER}/git/bitcoin-utils/setbtccontext.sh "$1"
+    #Sets BITCOIN_CHAIN and BITCOIN_GIT_BRANCH
+    eval $(python ./branch-manager.py context "$1")
+}
+
+network() {
+    export BITCOIN_
+}
+
+show_envar() {
+    #Shows the current environment variables
+    echo "GITDIR: $GITDIR"
+    echo "BITCOIN_BUILDDIR: $BITCOIN_BUILDDIR"
+    echo "BITCOIN_CHAIN: $BITCOIN_CHAIN"
+    echo "BITCOIN_GIT_BRANCH: $BITCOIN_GIT_BRANCH"
+}
+
+configure() {
+    cd $GITDIR
+    make clean
+    git clean -f -dX
+    mkdir build
+    cd build
+    ../autogen.sh
+    ../configure --without-bdb --without-gui
 }
 
 build() {
+    configure()
+    cd $GITDIR/build/
+    make -j 8
+    make check
+    dirname=${BITCOIN_BUILDDIR}/${BITCOIN_GIT_BRANCH}
+    echo "Moving to build directory:" $dirname
+    mkdir -p "$dirname"
+    mv -v /Users/${USER}/git/bitcoin/build/* "$dirname"/
+    cd $dirname
 
 }
 
+set_alias() {
+    alias btcdir="cd ~/.bitcoin/" #linux default bitcoind path
+    alias bc="bitcoin-cli -chain=${BITCOIN_CHAIN}" #linux default bitcoind path
+    alias bd="bitcoind -chain=${BITCOIN_CHAIN}" #linux default bitcoind path
+    alias btcinfo='bitcoin-cli getwalletinfo | egrep "\"balance\""; bitcoin-cli -chain=${BITCOIN_CHAIN} getnetworkinfo | egrep "\"version\"|connections"; bitcoin-cli -chain=${BITCOIN_CHAIN} getmininginfo | egrep "\"blocks\"|errors"'
 
+    alias bcstart="bitcoind -${BITCOIN_CHAIN} -daemon"
+    alias bcstop="bitcoin-cli -${BITCOIN_CHAIN} stop"
+    alias bcrestart="bitcoin-cli -${BITCOIN_CHAIN} stop; bitcoind -${BITCOIN_CHAIN} -daemon"
+    alias bcconf="bitcoin-cli -${BITCOIN_CHAIN} getinfo"
+
+    alias bcdir="cd /Users/${USER}/Library/Application Support/Bitcoin" #maco default bitcoin path
+    alias bcgit="cd /Users/${USER}/git/bitcoin"
+
+
+
+}
 
 pull() {
 
@@ -18,8 +71,4 @@ exit() {
 
 }
 
-
-alias btcdir="cd ~/.bitcoin/" #linux default bitcoind path
-
-alias bcstart="bitcoind ${NETWORK} -daemon"
-alias btcinfo='bitcoin-cli getwalletinfo | egrep "\"balance\""; bitcoin-cli getnetworkinfo | egrep "\"version\"|connections"; bitcoin-cli getmininginfo | egrep "\"blocks\"|errors"'
+set_alias
